613eabadcf62778b565ed2acdd67e4350722d981
PushpakB3096 <rtpushpak@gmail.com>
2021-06-12 10:00:26 +0530

    Add firebase config

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,16 +1,21 @@
-import { GoogleOutlined, FacebookOutlined } from "@ant-design/icons";
+import { GoogleOutlined } from "@ant-design/icons";
+import firebase from "firebase/app";
+import "firebase/app";
+
+import { auth } from "../firebase";
 
 const Login = () => {
   return (
     <div id='login-page'>
       <div id='login-card'>
         <h3>Welcome to ChatterBox!</h3>
-        <div className='login-button google'>
+        <div
+          className='login-button google'
+          onClick={() =>
+            auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())
+          }
+        >
           <GoogleOutlined /> Sign in with Google
-        </div>
-        <br /> <br />
-        <div className='login-button facebook'>
-          <FacebookOutlined /> Sign in with Facebook
         </div>
       </div>
     </div>
