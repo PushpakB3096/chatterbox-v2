be67644938be88629f95d2d3211bbe747708158b
PushpakB3096 <rtpushpak@gmail.com>
2021-06-12 11:20:56 +0530

    Add logout function

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -4,11 +4,22 @@ import { ChatEngine } from "react-chat-engine";
 import { auth } from "../firebase";
 
 const Chats = () => {
+  const history = useHistory();
+
+  // handles user logout
+  const handleLogout = async () => {
+    // this will logout the current logged in user
+    await auth.signOut();
+    // navigate user back to login page
+    history.push("/");
+  };
   return (
     <div className='chats-page'>
       <div className='nav-bar'>
         <div className='logo-tab'>ChatterBox</div>
-        <div className='logout-tab'>Logout</div>
+        <div className='logout-tab' onClick={() => handleLogout()}>
+          Logout
+        </div>
       </div>
 
       {/* TODO: move inline style to css */}
