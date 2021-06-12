47e32eb93ae9f44a53d7b9ccb513a2991de1b1d5
PushpakB3096 <rtpushpak@gmail.com>
2021-06-12 11:18:15 +0530

    Add chat engine

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,4 +1,7 @@
-import React from "react";
+import { useHistory } from "react-router-dom";
+import { ChatEngine } from "react-chat-engine";
+
+import { auth } from "../firebase";
 
 const Chats = () => {
   return (
@@ -7,6 +10,15 @@ const Chats = () => {
         <div className='logo-tab'>ChatterBox</div>
         <div className='logout-tab'>Logout</div>
       </div>
+
+      {/* TODO: move inline style to css */}
+      <ChatEngine
+        height='calc(100vh - 66px)'
+        projectId={process.env.REACT_APP_CHAT_ENGINE_PROJECT_ID}
+        // TODO: add username and secret
+        userName='.'
+        userSecret='.'
+      />
     </div>
   );
 };
